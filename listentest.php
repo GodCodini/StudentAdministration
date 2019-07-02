e26f6b99160122d0b734d9e1dfe976e69ebedf77
GodCodini <51437569+GodCodini@users.noreply.github.com>
2019-07-02 16:25:30 +0200

    readlist works, create new obj when add a node

--- listentest.php
+++ listentest.php
@@ -1,97 +1,139 @@
+<head>
+    <link rel="stylesheet" href="style/styles.css">
+</head>
+
 <?php
-class DoublyLinkedList {
+require 'functions.php';
+
+class DoublyLinkedList
+{
     private $start = null;
     private $end = null;
     private $count = 0;
-    //TODO Add-Funktion überarbeiten, verkettet rekursiv, sollte linear
-    public function add(Element $element) {
+
+    /**
+     * @param $data
+     */
+    public function add($data)
+    {
+        $element = new Element($data);
         //das erste element bekommt den start und endpunkt
-        if($this->start === null) {
+        if ($this->start === null) {
             $this->start = $element;
             $this->end = $element;
             $this->count++;
             return;
         }
-
         //ende auf das neue element setzen, wenn schon eins existiert.
         $this->end->setNext($element);
         $element->setPrevious($this->end);
-        $this->current = $element;
         $this->end = $element;
         $this->count++;
     }
 
-    public function getCount() {
+    public function getCount()
+    {
         return $this->count;
     }
-    //TODO Daten aus der Liste wiederbekommen
-    public function readList() {
+
+    public function readList()
+    {
         $start = $this->getStart();
         $test = $start->getData();
         $count = $this->getCount();
+        $arr = [];
         for ($i = 0; $i < $count; $i++) {
-            echo $test;
-            $test = $test->getNext();
+            array_push($arr, $test);
+            //echo $test . "<br>";
+            //verhindert ein getData auf ein leeres Objekt
+            if ($i != $count - 1) {
+                $start = $start->getNext();
+                $test = $start->getData();
+            }
+
         }
+        return $arr;
     }
 
-    public function getStart() {
+    public function getStart()
+    {
         return $this->start;
     }
 
-    public function getEnd() {
+    public function getEnd()
+    {
         return $this->end;
     }
 }
 
-class Element {
+/**
+ * Class Element
+ */
+class Element
+{
     private $prev;
     private $next;
     private $data;
 
-    public function __construct($data) {
+    public function __construct($data)
+    {
         $this->data = $data;
     }
 
-    public function setPrevious(Element $element) {
+    public function setPrevious(Element $element)
+    {
         $this->prev = $element;
     }
 
-    public function setNext(Element $element) {
+    public function getPrevious()
+    {
+        return $this->prev;
+    }
+
+    public function setNext(Element $element)
+    {
         $this->next = $element;
     }
 
-    public function getNext() {
+    public function getNext()
+    {
         return $this->next;
     }
 
-    public function setData($data) {
+    public function setData($data)
+    {
         $this->data = $data;
     }
 
-    public function getData() {
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function __toString()
+    {
         return $this->data;
     }
 }
 
-$elm1 = new Element("element 1");
-$elm2 = new Element("Das ist ein Test lol 2");
-$elm3 = new Element("testetstetstetstetstetstetst 3");
-$elm4 = new Element("teste");
-$elm5 = new Element("reeeeeeeeeeeeeeeee");
-
-
+$elm1 = "element 1";
+$elm2 = "Das ist ein Test lol 2";
+$elm3 = "testetstetstetstetstetstetst 3";
+$elm4 = "teste 4";
+$elm5 = "reeeeeeeeeeeeeeeee 5";
 $liste = new DoublyLinkedList();
+
 $liste->add($elm1);
 $liste->add($elm2);
 $liste->add($elm3);
 $liste->add($elm4);
 $liste->add($elm5);
-/*echo "<pre>";
-var_dump($liste);
-echo "</pre>";*/
+
 echo $liste->getCount();
+
 echo "<br>";
 echo "<pre>";
-var_dump($liste);
+highlight_string("<?php\n\$liste =\n" . var_export($liste, true) . ";\n?>");
 echo "</pre>";
+$array = $liste->readList();
+dev::printTableFromObjectArray($array);
