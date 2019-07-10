c25b3ec6c2607c7f89e7faa2d8e1d983841d2585
GodCodini <51437569+GodCodini@users.noreply.github.com>
2019-07-10 16:26:47 +0200

    first very simple ui for adding students to a list

--- index.php
+++ index.php
@@ -1,92 +1,47 @@
 <?php
-include 'db.php';
-include 'project_files/class_lib.php';
+include_once './ajaxNode.php';
 
-if (isset($_POST['submit'])) {
-    $vorname = $_POST['vorname'];
-    $nachname = $_POST['nachname'];
-    $geburtstdatum = $_POST['bday'];
-    $klasse = $_POST['kurs'];
 
-    $succsess = Schueler::create($vorname, $nachname, $geburtstdatum, $klasse);
-    
-}
 ?>
 
-<html>
-    <head>
-        <title>Schülerverwaltung</title>
-        <link rel="stylesheet" href="styles/styles.css">
-        <link rel="text/javascript" href="functions/functions.js">
-        <link href="jquery-ui-1.12.1/jquery-ui.css" rel="stylesheet">
-        <script src="jquery-ui-1.12.1/external/jquery/jquery.js"></script>
-        <script src="jquery-ui-1.12.1/jquery-ui.min.js"></script>
-        <script>
-            $( function() {
-                $( "#tabs" ).tabs();
-            } );
-        </script>
-    </head>
-    <body>
-        <div id="tabs">
-        <ul>
-            <li><a href="#tabs-1">Startseite</a></li>
-            <li><a href="#tabs-2">Schüler verwalten</a></li>
-            <li><a href="#tabs-3">Kurse/Klassen verwalten</a></li>
-            
-        </ul>
-        <div class="container">
-            <div class="row">
-                <div id="tabs-1">
-                    <p>Test</p>
-                    <p>
-                        <?php //TODO Startseite einrichten und nett machen ?>
-                        Begrüßung<br>
-                        Fehlermeldungen
-                    </p>
-                    <form action="index.php" method="post">
-                        <label for="vorname">Vorname</label>
-                        <input type="text" name="vorname">
-                        <label for="nachname">Nachname</label>
-                        <input type="text" name="nachname">
-                        <label for="bday">Geburtstdatum</label>
-                        <input type="date" name="bday">
-                        <label for="kurs">Kurs auswählen</label>
-                        <select name="kurs" id="kurs">
-                            <option value="1">FI7S</option>
-                            <option value="2">FI8A</option>
-                        </select>
-                        <input type="submit" name="submit" value="Senden">
-                    </form>
-                    <?php 
-                        if ($succsess) {
-                            echo 'Schüler wurde angelegt.';
-                        }
-                        else {
-                            echo 'Schüler konnte nicht angelegt werden';
-                        }
-                    ?>
-                </div>
-                <div id="tabs-2">
-                    <?php //TODO Schülerverwaltung implementieren. Tabelle klickbar machen? ?>
-                    <p>Comming soon</p>
-                    <p>Schüler anlegen/bearbeiten <br>
-                       Noten vergeben <br>
-                       Schüler Kursen zuweisen<br>
-                       Noten von Schülern berechnen 
-                    </p>
-                </div>
-                <div id="tabs-3">
-                    <?php //TODO Kurs implementieren. Klassen neu anlegen + mehrere Schüler anlegen ?>
-                    <p>Test aber mit 3</p>
-                    <p>
-                        Kurse anlegen/bearbeiten<br>
-                        Notenschlüssel festlegen<br>
-                        Schüler Kursen zuweisen                    
-                    </p>
-                </div>
-            </div>
-        </div>
-        </div>
-    </body>
-</html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!--<link rel="stylesheet" href="style/styles.css">-->
+
+    <script type="text/javascript" src="functions.js"></script>
+
+</head>
+
+<body>
+
+<form action="./index.php" onsubmit="return false;" method="post">
+    <label for="data">Daten für die Liste</label>
+    <input class="test" type="text" name="liste" id="listenname">
+    <button onclick="listElement()">Senden</button>
+</form>
+
+<form action="./index.php" onsubmit="return false;" method="post">
+    <label for="data">Daten fürn Knoten</label>
+    <input class="test" type="text" name="data" id="name">
+    <button onclick="nodeElement()">Senden</button>
+</form>
+
+<p>Hier stehen (hoffentlich irgendwann) alle Schüler:</p>
+<?php
+
+
+
+if (isset($_POST['liste'])) {
+    $test = $_POST['liste'];
+    $liste = new DoublyLinkedList();
+}
+
+if (isset($_POST['data'])) {
+    $name = $_POST['data'];
+    $schueler = new Schueler($name);
+    $liste->add($schueler);
+    $liste->readList();
+}
+?>
+</body>
