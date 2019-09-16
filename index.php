a3ab30cc791e724e8485a5c9dc0ba3f453d89fb7
pamperin <lennart96n@gmail.com>
2019-09-16 16:30:32 +0200

    idk its been too long

--- index.php
+++ index.php
@@ -1,39 +1,49 @@
 <?php
-//$pw = $_COOKIE['password'];
-//$pdo = new PDO('mysql:host=localhost;dbname=schuelerverwaltung', 'root', '');$redirect_after_login = 'files_lp/liste.php';
-//
-//$sql = "SELECT (aktuellesPW) FROM passwort";
-//$statement = $pdo->query($sql);
-//$result = $statement->fetch(PDO::FETCH_ASSOC);
-//$aktuellesPW = $result['aktuellesPW'];
-//
-//if (isset($_GET['succsess'])) {
-//    if ($_GET['succsess'] == "pwupdated") {
-//        $remember_password = strtotime('+1 days');
-//        $sql = "SELECT (aktuellesPW) FROM passwort";
-//        $statement = $pdo->query($sql);
-//        $result = $statement->fetch(PDO::FETCH_ASSOC);
-//        $aktuellePW = $result['aktuellesPW'];
-//        setcookie("password", $aktuellePW, $remember_password);
-//        header('Refresh:5;url=./admin.php?succsess=pwupdate');
-//    }
-//} elseif (empty($_COOKIE['password'])) {
-//    //Wenn der Cookie leer ist oder das falsche Passwort hat, redirect zur login.php
-//    header('Location: files_lp/login.php');
-//    exit;
-//} elseif ($pw != $aktuellesPW) {
-//    header('Location: files_lp/login.php');
-//    exit;
-//} else {
-//    header("Location: files_lp/liste.php");
-//}
-//error_reporting(E_ERROR | E_PARSE);
+/**
+ * Copyright (c) 2019. Ralf Klaßen & Lennart Pamperin
+ * This Software is licensed under GPL 3.0.
+ * This program comes with ABSOLUTELY NO WARRANTY!
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions:
+ * https://github.com/TheAmazingCodini/StudentAdministration/blob/master/LICENSE
+ */
+
+$PDO = DB::load(DBHOST, DBNAME, DBUSERNAME, DBPASSWORD);
+$pw = $_COOKIE['password'];
+$redirect_after_login = 'files_lp/liste.php';
+
+$sql = "SELECT (aktuellesPW) FROM passwort";
+$statement = $PDO->query($sql);
+$result = $statement->fetch(PDO::FETCH_ASSOC);
+$aktuellesPW = $result['aktuellesPW'];
+//wenn das passwort geändert wurde, cookie aktualisieren
+if (isset($_GET['succsess'])) {
+    if ($_GET['succsess'] == "pwupdated") {
+        $remember_password = strtotime('+1 days');
+        $sql = "SELECT (aktuellesPW) FROM passwort";
+        $statement = $PDO->query($sql);
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        $aktuellePW = $result['aktuellesPW'];
+        setcookie("password", $aktuellePW, $remember_password);
+        header('Refresh:5;url=./admin.php?succsess=pwupdate');
+    }
+} elseif (empty($_COOKIE['password'])) {
+    //Wenn der Cookie leer ist oder das falsche Passwort hat, redirect zur login.php
+    header('Location: files_lp/login.php');
+    exit;
+} elseif ($pw != $aktuellesPW) {
+    header('Location: files_lp/login.php');
+    exit;
+} else {
+    header("Location: files_lp/liste.php");
+}
+error_reporting(E_ERROR | E_PARSE);
 require_once 'project_files/Database.php';
 require_once 'project_files/_config.php';
 include_once 'files_lp/ui/header.php';
 include_once 'files_lp/includes/functions.php';
 
-$PDO = DB::load(DBHOST, DBNAME, DBUSERNAME, DBPASSWORD);
+
 
 if (isset($_GET['id']))
 {
